820171b0b4138d859da60c486d55ef6c1b774b7f
mariacharina17 <charina.ortiz@yahoo.com>
2019-11-09 08:06:10 +0800

    Homework Answers

---
+++ exercise07.py
@@ -0,0 +1,10 @@
+class Car:
+    def _init_(self, year, make, model):
+        self.year = year
+        self.make = make
+        self. model = model
+
+def car_age(self):
+    return (2019 - year)
+
+
